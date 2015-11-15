d6f40742b416de7c7a4205f0496b488bfb164c2d
Youssef Kababe <hello@youssefkababe.com>
2015-11-15 18:04:29 +0000

    Update serender.js

--- serender.js
+++ serender.js
@@ -9,7 +9,7 @@ module.exports = function(req, res, next) {
   Browser.localhost('localhost', req.app.get('port'))
   var browser = new Browser()
   browser.headers = req.headers
-  browser.headers['serender'] = true
+  browser.headers.serender = true
 
   browser.visit(req.originalUrl, function() {
     res.set(browser.response.headers.toObject())
